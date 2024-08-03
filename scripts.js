96e25b45bb8b036daf55c4c1341686906c0aa2c4
Álvaro Aledo Tornero <alvaroaledotornero@gmail.com>
2024-08-03 20:23:50 +0200

    corrección foto de inicio

--- scripts.js
+++ scripts.js
@@ -9,19 +9,19 @@ document.addEventListener('DOMContentLoaded', function () {
     grabCursor: true,
     spaceBetween: 30,
   
-    // If we need pagination
+    
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
         dynamicBullets: true
     },
   
-    // Navigation arrows
+    
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-    // Responsive breakpoints
+    
     breakpoints:{
         0:{
         slidesPerView: 1,
@@ -39,6 +39,47 @@ document.addEventListener('DOMContentLoaded', function () {
   
   });
 });  
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const img = document.getElementById('foto-alvaro');
+  let originalBorderColor = getComputedStyle(img).borderColor;
+  let interval;
+
+  img.addEventListener('click', () => {
+      if (interval) return; 
+
+      let r = 255, g = 0, b = 0;
+      let step = 5; 
+
+      interval = setInterval(() => {
+          
+          img.style.borderColor = `rgb(${r}, ${g}, ${b})`;
+
+          
+          if (r === 255 && g < 255 && b === 0) {
+              g += step;
+          } else if (g === 255 && r > 0 && b === 0) {
+              r -= step;
+          } else if (g === 255 && b < 255 && r === 0) {
+              b += step;
+          } else if (b === 255 && g > 0 && r === 0) {
+              g -= step;
+          } else if (b === 255 && r < 255 && g === 0) {
+              r += step;
+          } else if (r === 255 && b > 0 && g === 0) {
+              b -= step;
+          }
+
+          
+          if (r === 255 && g === 0 && b === 0) {
+              clearInterval(interval);
+              interval = null;
+              img.style.borderColor = originalBorderColor;
+          }
+      }, 1);
+  });
+});
 
 
 
