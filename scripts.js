4d06bc9a96f0f66b86b751786440c6aa359b580b
Álvaro Aledo Tornero <alvaroaledotornero@gmail.com>
2024-08-03 18:30:24 +0200

    Swipper de proyectos implementado

--- scripts.js
+++ scripts.js
@@ -1,5 +1,47 @@
 
 
+        
+
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.slider-proyectos', {
+
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+  
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    // Responsive breakpoints
+    breakpoints:{
+        0:{
+        slidesPerView: 1,
+        spaceBetween: 20
+        },
+        620:{
+        slidesPerView: 2,
+        spaceBetween: 20
+        },
+        1024:{
+        slidesPerView: 3,
+        spaceBetween: 20
+        }
+    }
+  
+  });
+});  
+
+
+
 
 function topFunction() {
   
